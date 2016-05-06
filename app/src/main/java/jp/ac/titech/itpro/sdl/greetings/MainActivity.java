8be05ff322ca57c0d839a0f9e01de9f8d9783ae0
ke-sasada <sasada.k.ab@m.titech.ac.jp>
2016-05-06 13:39:57 +0900

    First Commit * change auther

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -0,0 +1,36 @@
+package jp.ac.titech.itpro.sdl.greetings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextView outputView;
+    private EditText inputName;
+    private Button okButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        outputView = (TextView)findViewById(R.id.output_view);
+        inputName = (EditText) findViewById(R.id.input_name);
+        okButton = (Button) findViewById(R.id.ok_button);
+    }
+
+    public void greet(View v){
+        String name = inputName.getText().toString();
+        if(name.length() > 0){
+            outputView.setText("Hello, " + name + "\nNice to see you!");
+        }
+    }
+
+
+}
